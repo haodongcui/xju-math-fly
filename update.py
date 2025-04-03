e9b2ee4db5e958466a091ccf7b46cac1fd359efa
haodongcui <2081823899@qq.com>
2025-04-04 03:13:35 +0800

    dist -> docs

--- update.py
+++ update.py
@@ -6,11 +6,11 @@ from utils.copy_to_page import *
 
 # 生成资源页面
 resource_path = '课程资料'
-resource_html_path = 'dist/resource.html'
+resource_html_path = 'docs/resource.html'
 base_url = "https://haodongcui.github.io/xju-math-fly/课程资料"
 generate_resource_html(resource_path, resource_html_path, base_url)
 
 # 复制resource.html为index.html
-source_path = 'dist/resource.html'
-target_path = 'dist/index.html'
+source_path = 'docs/resource.html'
+target_path = 'docs/index.html'
 copy_and_rename(source_path, target_path)
