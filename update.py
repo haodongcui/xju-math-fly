1bc9c183c78a98ed5a941229b7eb197848664e4d
cuihd <122677642+haodongcui@users.noreply.github.com>
2025-04-07 14:04:55 +0800

    Update update.py url

--- update.py
+++ update.py
@@ -7,7 +7,7 @@ from utils.copy_to_page import *
 # 生成资源页面
 resource_path = '课程资料'
 resource_html_path = 'docs/resource.html'
-base_url = "https://haodongcui.github.io/xju-math-fly/课程资料"
+base_url = "https://github.com/haodongcui/xju-math-fly/课程资料"
 generate_resource_html(resource_path, resource_html_path, base_url)
 
 # 复制resource.html为index.html
